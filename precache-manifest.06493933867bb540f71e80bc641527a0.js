fa654a0ec85dbaa02316ab56f13494bf3d9ba75d
Asmir Islamovic <asmir.islamovic@mail.utoronto.ca>
2020-08-28 18:57:02 -0400

    Updates

---
+++ precache-manifest.06493933867bb540f71e80bc641527a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0730012cf5f16c69ca00911843b7484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78c7b5be02ebe0cf3651",
+    "url": "/static/css/main.0906da70.chunk.css"
+  },
+  {
+    "revision": "cfe3db2edf09dc41a02c",
+    "url": "/static/js/2.2ece84c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2ece84c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c7b5be02ebe0cf3651",
+    "url": "/static/js/main.60cd8f46.chunk.js"
+  },
+  {
+    "revision": "49c613b23bd1ed6cb6cc",
+    "url": "/static/js/runtime-main.1dbf960d.js"
+  }
+]);
